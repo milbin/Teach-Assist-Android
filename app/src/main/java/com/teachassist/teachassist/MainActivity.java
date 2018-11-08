f4b72aa912bc92e994bbf24920b0ad6b3c8a9f90
Benjamin <Benjamintran0684@gmail.com>
2018-11-08 10:52:16 -0500

    added request sender class

--- app/src/main/java/com/teachassist/teachassist/MainActivity.java
+++ app/src/main/java/com/teachassist/teachassist/MainActivity.java
@@ -2,7 +2,9 @@ package com.teachassist.teachassist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import java.net.URLConnection;
+import java.net.URL;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     private void GetTAData(String Username, String Password) {
+        String url = "https://ta.yrdsb.ca/live/index.php";
+        String charset = "UTF-8";
+        String query = "";
+        URLConnection connection = new URL(url + "?" + query).openConnection();
+        connection.setDoOutput(true); // Triggers POST.
+        connection.setRequestProperty("Accept-Charset", charset);
+        InputStream response = connection.getInputStream();
+
+
+
 
     }
+
 }
