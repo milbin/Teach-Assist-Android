5e6fa5c4074bec24715d3c1e48a381405baa1278
Benjamin <Benjamintran0684@gmail.com>
2018-12-03 21:29:54 -0500

    added toolbar to settingsView

--- app/src/main/java/com/teachassist/teachassist/SettingsActivity.java
+++ app/src/main/java/com/teachassist/teachassist/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.teachassist.teachassist;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
@@ -7,14 +8,16 @@ import android.preference.PreferenceFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.widget.Toolbar;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
 
         //load settings fragment
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.dynamic_fragment_frame_layout,
                 new PrefsFragment()).commit();
     }
 
